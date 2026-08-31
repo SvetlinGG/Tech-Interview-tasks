370bed3911ce93def8d477f28192ec943b0cca9f
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-31 22:20:32 +0200

    solve the task

--- medium/array/find-the-missing-number.js
+++ medium/array/find-the-missing-number.js
@@ -1,5 +1,18 @@
 function findMissing(arr, n){
 
+    let newArr = [];
+    let missingNumber;
+    for (let i = 1; i <= n; i++){
+        newArr.push(i);
+
+        if ( !arr.includes(i)){
+            missingNumber = i;
+            //newArr.splice(i, 0, missingNumber)
+        }
+    }
+    console.log(missingNumber);
+    
+
 
 }
 findMissing([1, 2, 3, 5, 6], 6);
