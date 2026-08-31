73a9fd88d22d9251b4d04a4ebe399d2787d41823
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-31 22:01:33 +0200

    find unique element

---
+++ easy/strings/remove-duplicates.js
@@ -0,0 +1,14 @@
+function removeDuplicates(arr){
+    const newArr = [];
+    const found = arr.find((element, index) => {
+        if (element !== index){
+            newArr.push(element)
+
+        }
+    })
+    console.log(newArr);
+    
+
+}
+removeDuplicates([1, 2, 2, 3, 4, 4, 5])
+// [1, 2, 3, 4, 5]
