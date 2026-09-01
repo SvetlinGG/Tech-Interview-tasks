48ef7d1bc441e282e618d53753a578097ada500b
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-01 23:06:45 +0200

    create item object

--- hard/objects/shopping-cart.js
+++ hard/objects/shopping-cart.js
@@ -1,6 +1,33 @@
 function calculateCart(cart){
 
+    let completeObj = {
+        totalItems: 0,
+        totalPrice: 0,
+        mostExpensive: ""
+    };
 
+    let itemsInfo = Object.entries(cart);
+    
+    for ( let items of itemsInfo){
+        //console.log(items[1]);
+        
+        for ( let content of Object.entries(items[1])){
+            //console.log(content);
+            let [keyItems, valueItems] = content;
+            for ( let i = 0; i < valueItems; i+=2){
+                let totalItems = valueItems[i]
+                if(!completeObj[totalItems]){
+                completeObj[totalItems] += Number(valueItems[i])  
+            }
+
+            }
+            
+            
+        }
+        
+    }
+    console.log(completeObj);
+    
 }
 calculateCart([
     { name: "Laptop", price: 1200, quantity: 1 },
