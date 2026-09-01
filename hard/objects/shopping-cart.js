c51357b7ed94985192a8b6de2f95fe2aa3254f9a
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-01 21:35:47 +0200

    shoping cart

---
+++ hard/objects/shopping-cart.js
@@ -0,0 +1,17 @@
+function calculateCart(cart){
+
+
+}
+calculateCart([
+    { name: "Laptop", price: 1200, quantity: 1 },
+    { name: "Mouse", price: 25, quantity: 2 },
+    { name: "Keyboard", price: 80, quantity: 1 },
+    { name: "Monitor", price: 300, quantity: 2 }
+])
+
+// Output 
+// {
+//     totalItems: 6,
+//     totalPrice: 1930,
+//     mostExpensive: "Laptop"
+// }
