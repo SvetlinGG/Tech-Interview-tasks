a3a280891a6fc48d6fe50facbb37b19d2b3217a9
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-01 11:11:48 +0200

    fix the issue

--- medium/string/first-non-repeat-character.js
+++ medium/string/first-non-repeat-character.js
@@ -1,19 +1,29 @@
 function firstUniqueChar(str){
 
+    let char = {}
     let word = str.split('');
 
-    let arr = [];
-    word.map((el, index) => {
-        if(el !== index){
-            arr.push(el)
-        }
-    })
-    console.log(arr);
-    
+    for ( let i = 0; i < word.length; i++){
+         let key = word[i];
 
+        if (!char[key]){
+            char[key] = 1
+        }else{
+            char[key]++;
+        }
+    }
+    
+    let countChar = Object.entries(char)
+    for ( let [key, value] of countChar){
+        if (value === 1){
+            return key;
+            
+        }
+    }
+    return null
 }
-firstUniqueChar("aabbcddee")
+console.log(firstUniqueChar("aabbcddee"))
 // "c"
 
-firstUniqueChar("aabbcc")
+console.log(firstUniqueChar("aabbcc"))
 // null
