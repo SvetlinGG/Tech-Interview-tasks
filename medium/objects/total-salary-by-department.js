39bbb7e985baf781469f8a1c276115f964fda378
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-02 22:41:27 +0200

    total salary by department

---
+++ medium/objects/total-salary-by-department.js
@@ -0,0 +1,15 @@
+function salaryByDepartment(employees){
+
+
+}
+salaryByDepartment([
+    { name: "Anna", department: "IT", salary: 2000 },
+    { name: "John", department: "HR", salary: 1500 },
+    { name: "Peter", department: "IT", salary: 2500 },
+    { name: "Maria", department: "HR", salary: 1700 }
+]);
+
+// {
+//     IT: 4500,
+//     HR: 3200
+// }
