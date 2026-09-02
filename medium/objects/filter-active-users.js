373ee8a201f6b1858a7d44d1ab79801727dcd776
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-02 22:37:57 +0200

    use map function

--- medium/objects/filter-active-users.js
+++ medium/objects/filter-active-users.js
@@ -1,8 +1,25 @@
 function getActiveUsers(users){
 
     let arr = [];
-    for ( let item of users){
+
+    users.map(item => {
+        if (item.active === true) arr.push(item.name)
+
+    })
+    // for ( let item of users){
         
-        if ( item.active === true){
-            arr.push(item.name)
-      
+    //     if ( item.active === true){
+    //         arr.push(item.name)
+    //     }
+    // }
+    console.log(arr);
+    
+
+
+}
+getActiveUsers([
+    { name: "Anna", active: true },
+    { name: "John", active: false },
+    { name: "Peter", active: true }
+])
+// ["Anna", "Peter"]
