3d2dbe7519e779035212825882689c4f75aef8cb
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-03 15:58:32 +0200

    fix some issues

--- medium/objects/find-user-by-id.js
+++ medium/objects/find-user-by-id.js
@@ -1,24 +1,25 @@
 function findUser(users, id){
 
-    
+//     users = [
+//     { id: 1, name: "Anna" },
+//     { id: 2, name: "John" },
+//     { id: 3, name: "Peter" }
+// ];
+
 for ( let user of users){
 
-    if (user.id == 2){
-        return user.name;
-    }else {
-        return null;
-        
+    if (user.id === id){
+        console.log(`{ id:${user.id}, name:${user.name} }`);
     }
+    return null;
 }
-
-
 
 }
 findUser([
     { id: 1, name: "Anna" },
     { id: 2, name: "John" },
     { id: 3, name: "Peter" }
-]);
+], 3);
 // { id: 2, name: "John" };
 
 //findUser(users, 10)
