ba6e6dfd0761459e0c4ba6df2ab9f57c5c43dcc9
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-03 23:47:42 +0200

    use again math.max

--- medium/string/find-longest-word.js
+++ medium/string/find-longest-word.js
@@ -2,7 +2,7 @@ function findLongestWord(str){
 
     let arr = str.split(' ');
     let result = arr.map(word => {
-        if (word.length  === arr.find(word)) {
+        if (word.length  === Math.max(...arr.map(word => word.length))) {
             console.log(word);
             
         }
