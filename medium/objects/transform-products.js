328941b0608d52c2237aad268cce2ef0456edd5e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-03 22:23:23 +0200

    make discount but change existing array

--- medium/objects/transform-products.js
+++ medium/objects/transform-products.js
@@ -1,9 +1,13 @@
 function applyDiscount(products, discount){
 
     for ( let items of products){
+        if (items){
+            items.price = items.price - (items.price * discount / 100);
+            
+        }
         
     }
-
+    return products;
 
 }
 applyDiscount([
