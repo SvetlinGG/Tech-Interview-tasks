81a421f5b38b63dccfb07e0f045b93225c13aeff
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-04 23:10:30 +0200

    solve the task

---
+++ medium/objects/nested-order-total.js
@@ -0,0 +1,31 @@
+function calculateOrdersTotal(orders){
+
+    let sum = 0;
+
+    for ( let order of orders){
+        for ( let item of order.items){
+            sum += item.price * item.quantity;
+
+        }
+    }
+    return sum;
+    
+
+
+}
+calculateOrdersTotal([
+    {
+        id: 1,
+        items: [
+            { name: "Laptop", price: 1000, quantity: 1 },
+            { name: "Mouse", price: 20, quantity: 2 }
+        ]
+    },
+    {
+        id: 2,
+        items: [
+            { name: "Keyboard", price: 80, quantity: 1 }
+        ]
+    }
+]);
+// 1120
