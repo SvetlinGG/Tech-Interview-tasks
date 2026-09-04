ef61f60a14b6f36a4c76eec8ae5e4819c712afe5
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-04 10:16:07 +0200

    count product by category

---
+++ medium/objects/count-product-by-category.js
@@ -0,0 +1,11 @@
+function countByCategory(products){
+
+
+}
+countByCategory([
+    { name: "Laptop", category: "tech" },
+    { name: "Mouse", category: "tech" },
+    { name: "Apple", category: "food" },
+    { name: "Bread", category: "food" },
+    { name: "Phone", category: "tech" }
+])
