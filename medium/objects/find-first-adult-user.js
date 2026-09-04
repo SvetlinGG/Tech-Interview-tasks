e936c41e2ec3f43b0b126152771dbba630959d69
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-04 20:38:05 +0200

    find first adult user

---
+++ medium/objects/find-first-adult-user.js
@@ -0,0 +1,10 @@
+function findFirstAdult(users){
+
+
+}
+findFirstAdult([
+    { name: "Anna", age: 16 },
+    { name: "John", age: 17 },
+    { name: "Peter", age: 22 },
+    { name: "Maria", age: 30 }
+])
