ca1b29a88ac2317526161f0075a97d4c5334773b
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-04 22:39:50 +0200

    solve the task

--- medium/array/group-numbers.js
+++ medium/array/group-numbers.js
@@ -1,5 +1,12 @@
 function groupNumbers(arr){
 
+    let numObj = {
+        odd: arr.filter(num => num % 2 !== 0),
+        even: arr.filter(num => num % 2 === 0)
+    }
+    return numObj;
+    
+
 
 }
 groupNumbers([1, 2, 3, 4, 5, 6])
