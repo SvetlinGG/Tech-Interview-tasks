946333d5efb2c06863c5c4c09f5a90c5dbb1b668
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-06 01:50:02 +0200

    solve the task

--- medium/array/unique-words-sorted-by-frequency.js
+++ medium/array/unique-words-sorted-by-frequency.js
@@ -1,5 +1,23 @@
 function wordFrequency(text){
 
+    let newText = text.split(' ').map(word => word.toLowerCase());
+
+    let textObj = {};
+    for ( let word of newText){
+        if (!textObj[word]){
+            textObj[word] = 1;
+            
+        }else{
+            textObj[word] ++;
+        }
+    }
+    let textArray = Object.entries(textObj).sort((a, b) => b[1] - a[1]);
+    return textArray;
+    
+    
+    
+    
+
 
 }
 wordFrequency("JS is great and JS is fast and JS is useful")
