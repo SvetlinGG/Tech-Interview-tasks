a079d8ae6ffc230ee377cecd1f3e1d961f304e1a
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-06 21:32:08 +0200

    card total

--- medium/objects/total-stock-value-by-category.js
+++ medium/objects/total-stock-value-by-category.js
@@ -26,6 +26,4 @@ function stockValueByCategory(products){
 stockValueByCategory([
     { name: "Laptop", category: "tech", price: 1200, stock: 2 },
     { name: "Mouse", category: "tech", price: 25, stock: 10 },
-    { name: "Apple", category: "food", price: 2, stock: 50 },
-    { name: "Bread", category: "food", price: 3, stock: 20 }
-])
+    { name: "Apple", category: "food", price: 2, s
