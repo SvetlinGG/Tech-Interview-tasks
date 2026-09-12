1b4ac7796d65429702ade123aad3d4cb34fc08a8
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-12 23:31:26 +0200

    nested data

---
+++ hard/arrays/03-nested-data.js
@@ -0,0 +1,35 @@
+function getMostExpensiveOrder(orders){
+
+
+}
+getMostExpensiveOrder([
+    {
+        id: 1,
+        customer: "Anna",
+        products: [
+            { name: "Laptop", price: 1200, quantity: 1 },
+            { name: "Mouse", price: 40, quantity: 2 }
+        ]
+    },
+    {
+        id: 2,
+        customer: "John",
+        products: [
+            { name: "Monitor", price: 300, quantity: 2 },
+            { name: "Keyboard", price: 80, quantity: 1 }
+        ]
+    },
+    {
+        id: 3,
+        customer: "Maria",
+        products: [
+            { name: "Phone", price: 700, quantity: 1 },
+            { name: "Case", price: 30, quantity: 3 }
+        ]
+    }
+]);
+
+// {
+//     customer: "Anna",
+//     total: 1280
+// }
