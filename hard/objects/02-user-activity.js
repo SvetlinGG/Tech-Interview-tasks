b26730b2eead591f4cc28d0022fef20d88d12d35
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-09-13 03:08:38 +0200

    first logical steps

--- hard/objects/02-user-activity.js
+++ hard/objects/02-user-activity.js
@@ -1,5 +1,20 @@
 function countUserActions(activities){
 
+    let userObj = {};
+
+    for (let user of activities){
+        if (userObj[user.user]){
+            if(user.action === 'login' || user.action === 'logout' || user.action === 'view'){
+                userObj[user.user][user.action] += user.action
+            }
+            
+        }else{
+            userObj[user.user] = user.action
+        }
+    }
+    console.log(userObj);
+    
+
 
 }
 countUserActions([
@@ -8,26 +23,4 @@ countUserActions([
     { user: "Anna", action: "view" },
     { user: "Peter", action: "login" },
     { user: "Anna", action: "logout" },
-    { user: "John", action: "view" },
-    { user: "Anna", action: "login" },
-    { user: "Peter", action: "view" },
-    { user: "John", action: "logout" }
-]);
-
-//result
-// {
-//     Anna: {
-//         login: 2,
-//         view: 1,
-//         logout: 1
-//     },
-//     John: {
-//         login: 1,
-//         view: 1,
-//         logout: 1
-//     },
-//     Peter: {
-//         login: 1,
-//         view: 1
-//     }
-// }
+    { user: "John", action: "view"
